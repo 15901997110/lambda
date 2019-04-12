23e4ce961f6b41b2ef61315354c34221b1444c62
鲁奇伟 <qiwei.lu@b-and-qchina.com>
2019-04-12 18:37:55 +0800

    多个字段GroupBy统计

--- src/main/java/com/edu/java8/lambda/Book.java
+++ src/main/java/com/edu/java8/lambda/Book.java
@@ -5,50 +5,77 @@ import java.time.LocalDate;
 public class Book {
 	private int id;
 	private String name;
+	private String author;
 	private double price;
 	private String type;
 	private LocalDate publishDate;
-	
-	public Book(int id, String name, double price, String type, LocalDate publishDate) {
+
+	public Book(int id, String name, String author, double price, String type, LocalDate publishDate) {
 		this.id = id;
 		this.name = name;
+		this.author = author;
 		this.price = price;
 		this.type = type;
 		this.publishDate = publishDate;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
+
 	public LocalDate getPublishDate() {
 		return publishDate;
 	}
+
 	public void setPublishDate(LocalDate publishDate) {
 		this.publishDate = publishDate;
 	}
+
 	@Override
 	public String toString() {
-		return "Book [id=" + id + ", name=" + name + ", price=" + price + ", type=" + type + ", publishDate="
-				+ publishDate + "]";
+		return "Book{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", author='" + author + '\'' +
+				", price=" + price +
+				", type='" + type + '\'' +
+				", publishDate=" + publishDate +
+				'}';
 	}
 }
--- src/test/java/com/edu/java8/lambda/LambdaTest.java
+++ src/test/java/com/edu/java8/lambda/LambdaTest.java
@@ -15,97 +15,97 @@ import org.junit.Test;
 
 public class LambdaTest {
 
-	/**
-	 * index.do?itemId=1&userId=10000&type=20&token=111111111111&key=index
-	 */
-	@Test
-	public void test1() {
-		String queryString = "itemId=1&userId=10000&type=20&token=111111111111&key=index";
-		Map<String, String> params = Stream.of(queryString.split("&")).map(str -> str.split("=")).collect(Collectors.toMap(s -> s[0], s -> s[1]));
-		System.out.println(params);
-	}
-	
-	@Test
-	public void test2 () {
-		List<Integer> ids = books().stream().map( book -> book.getId()).collect(Collectors.toList());
-		System.out.println(ids);
-		
-		ids = books().stream().map(Book::getId).collect(Collectors.toList());
-		System.out.println(ids);
-		
-		String str = books().stream().map(book -> book.getId()+"").collect(Collectors.joining(","));
-		System.out.println(str);
-		
-		str = books().stream().map(book -> book.getId()+"").collect(Collectors.joining(",", "(", ")"));
-		System.out.println(str);
-		
-		str = books().stream().map(book -> "'"+book.getId()+"'").collect(Collectors.joining(",", "(", ")"));
-		System.out.println(str);
-	}
-	
-	@Test
-	public void test3 () {
-		List<String> list = books().stream().map(Book::getType).collect(Collectors.toList());
-		System.out.println(list);
-		
-		list = books().stream().map(Book::getType).distinct().collect(Collectors.toList());
-		System.out.println(list);
-		
-		Set<String> set = books().stream().map(Book::getType).collect(Collectors.toSet());
-		System.out.println(set);
-	} 
-	
-	@Test
-	public void test4 () {
+    /**
+     * index.do?itemId=1&userId=10000&type=20&token=111111111111&key=index
+     */
+    @Test
+    public void test1() {
+        String queryString = "itemId=1&userId=10000&type=20&token=111111111111&key=index";
+        Map<String, String> params = Stream.of(queryString.split("&")).map(str -> str.split("=")).collect(Collectors.toMap(s -> s[0], s -> s[1]));
+        System.out.println(params);
+    }
+
+    @Test
+    public void test2() {
+        List<Integer> ids = books().stream().map(book -> book.getId()).collect(Collectors.toList());
+        System.out.println(ids);
+
+        ids = books().stream().map(Book::getId).collect(Collectors.toList());
+        System.out.println(ids);
+
+        String str = books().stream().map(book -> book.getId() + "").collect(Collectors.joining(","));
+        System.out.println(str);
+
+        str = books().stream().map(book -> book.getId() + "").collect(Collectors.joining(",", "(", ")"));
+        System.out.println(str);
+
+        str = books().stream().map(book -> "'" + book.getId() + "'").collect(Collectors.joining(",", "(", ")"));
+        System.out.println(str);
+    }
+
+    @Test
+    public void test3() {
+        List<String> list = books().stream().map(Book::getType).collect(Collectors.toList());
+        System.out.println(list);
+
+        list = books().stream().map(Book::getType).distinct().collect(Collectors.toList());
+        System.out.println(list);
+
+        Set<String> set = books().stream().map(Book::getType).collect(Collectors.toSet());
+        System.out.println(set);
+    }
+
+    @Test
+    public void test4() {
 //		books().stream().sorted((book1, book2) -> Double.compare(book1.getPrice(), book2.getPrice())).forEach(System.out::println);;
-		
+
 //		Comparator<Book> compa = (book1, book2) -> Double.compare(book1.getPrice(), book2.getPrice());
 //		books().stream().sorted(compa.reversed()).forEach(System.out::println);
-	
+
 //		Comparator<Book> compa = (book1, book2) -> Double.compare(book1.getPrice(), book2.getPrice());
 //		books().stream().sorted(compa.thenComparing((book1,book2) -> book1.getPublishDate().isAfter(book2.getPublishDate()) ? -1 : 1)).forEach(System.out::println);
-		
+
 //		books().stream().sorted(Comparator.comparing(Book::getPrice)).forEach(System.out::println);
 //		books().stream().sorted(Comparator.comparing(Book::getPrice).reversed()).forEach(System.out::println);
-		books().stream().sorted(Comparator.comparing(Book::getPrice).reversed().thenComparing(Comparator.comparing(Book::getPublishDate).reversed())).forEach(System.out::println);
-	}
-	
-	@Test
-	public void test5 () {
+        books().stream().sorted(Comparator.comparing(Book::getPrice).reversed().thenComparing(Comparator.comparing(Book::getPublishDate).reversed())).forEach(System.out::println);
+    }
+
+    @Test
+    public void test5() {
 //		Map<Integer, Book> booksMap = books().stream().collect(Collectors.toMap(book -> book.getId(), book -> book));
 //		System.out.println(booksMap);
-		
-		Map<Integer, Book> booksMap = books().stream().collect(Collectors.toMap(Book::getId, book -> book));
-		System.out.println(booksMap);
-	}
-	
-	@Test
-	public void test6 () {
-		Double avg = books().stream().collect(Collectors.averagingDouble(Book::getPrice));
-		System.out.println(avg);
-	}
-	
-	@Test
-	public void test7 () {
-		Optional<Book> book = books().stream().collect(Collectors.maxBy(Comparator.comparing(Book::getPrice)));
-		System.out.println(book);
-		
-		book = books().stream().collect(Collectors.minBy(Comparator.comparing(Book::getPrice)));
-		System.out.println(book);
-		
-		book = books().stream().collect(Collectors.minBy(Comparator.comparing(Book::getPublishDate)));
-		System.out.println(book);
-		
-		book = books().stream().collect(Collectors.maxBy(Comparator.comparing(Book::getPublishDate)));
-		System.out.println(book);
-		
-		Comparator<Book> comp = Comparator.comparing(Book::getPrice);
-		book = books().stream().collect(Collectors.maxBy(comp.thenComparing(Comparator.comparing(Book::getPublishDate))));
-		System.out.println(book);
-	}
-	
-	@Test
-	public void test8 () {
+
+        Map<Integer, Book> booksMap = books().stream().collect(Collectors.toMap(Book::getId, book -> book));
+        System.out.println(booksMap);
+    }
+
+    @Test
+    public void test6() {
+        Double avg = books().stream().collect(Collectors.averagingDouble(Book::getPrice));
+        System.out.println(avg);
+    }
+
+    @Test
+    public void test7() {
+        Optional<Book> book = books().stream().collect(Collectors.maxBy(Comparator.comparing(Book::getPrice)));
+        System.out.println(book);
+
+        book = books().stream().collect(Collectors.minBy(Comparator.comparing(Book::getPrice)));
+        System.out.println(book);
+
+        book = books().stream().collect(Collectors.minBy(Comparator.comparing(Book::getPublishDate)));
+        System.out.println(book);
+
+        book = books().stream().collect(Collectors.maxBy(Comparator.comparing(Book::getPublishDate)));
+        System.out.println(book);
+
+        Comparator<Book> comp = Comparator.comparing(Book::getPrice);
+        book = books().stream().collect(Collectors.maxBy(comp.thenComparing(Comparator.comparing(Book::getPublishDate))));
+        System.out.println(book);
+    }
+
+    @Test
+    public void test8() {
 //		Map<String, List<Book>> booksMap = books().stream().collect(Collectors.groupingBy(Book::getType));
 //		booksMap.keySet().forEach(key -> {
 //			System.out.println(key);
@@ -115,51 +115,61 @@ public class LambdaTest {
 //		
 //		Map<String, Long> booksCount = books().stream().collect(Collectors.groupingBy(Book::getType, Collectors.counting()));
 //		System.out.println(booksCount);
-		
-//		Map<String, Double> booksSum = books().stream().collect(Collectors.groupingBy(Book::getType, Collectors.summingDouble(Book::getPrice)));
-//		System.out.println(booksSum);
-		
+
+        Map<String, Double> booksSum = books().stream().collect(Collectors.groupingBy(Book::getType, Collectors.summingDouble(Book::getPrice)));
+        System.out.println(booksSum);
+
 //		Map<String, Double> booksSum = books().stream().collect(Collectors.groupingBy(Book::getType, Collectors.averagingDouble(Book::getPrice)));
 //		System.out.println(booksSum);
 		
 /*		Map<String, Optional<Book>> booksMaxPrice = books().stream()
 				.collect(Collectors.groupingBy(Book::getType, Collectors.maxBy(Comparator.comparing(Book::getPrice))));
 		System.out.println(booksMaxPrice);*/
-		Collector<Book, ?, Map<String, Long>> bookMapCollector = Collectors.groupingBy(Book::getType, Collectors.counting());
+/*		Collector<Book, ?, Map<String, Long>> bookMapCollector = Collectors.groupingBy(Book::getType, Collectors.counting());
 		Map<String, Long> collect = books().stream().collect(bookMapCollector);
 		System.out.println(collect);
 		Optional<Long> max = collect.values().stream().max(Comparator.naturalOrder());
-		System.out.println(max);
+		System.out.println(max);*/
 //		Map<String, Optional<Book>> booksMinPrice = books().stream().collect(Collectors.groupingBy(Book::getType, Collectors.minBy(Comparator.comparing(Book::getPrice))));
 //		System.out.println(booksMinPrice);
 		
 /*		Map<String, Optional<Book>> booksMaxPubDate = books().stream().collect(Collectors.groupingBy(Book::getType, Collectors.maxBy(Comparator.comparing(Book::getPublishDate))));
 		System.out.println(booksMaxPubDate);*/
-	}
-	
-	@Test
-	public void test9 () {
-		books().stream().filter(book -> book.getPrice() >= 80).sorted(Comparator.comparing(Book::getPublishDate).reversed()).forEach(System.out::println);;
-	}
-	
-	private List<Book> books(){
-		List<Book> books = new ArrayList<>();
-		books.add(new Book(1, "tomcat", 70d, "服务器", LocalDate.parse("2014-05-17")));
-		books.add(new Book(2, "jetty", 60d, "服务器", LocalDate.parse("2015-12-01")));
-		books.add(new Book(3, "nginx", 65d, "服务器", LocalDate.parse("2016-10-17")));
-		books.add(new Book(4, "java", 66d, "编程语言", LocalDate.parse("2011-04-09")));
-		books.add(new Book(5, "ruby", 80d, "编程语言", LocalDate.parse("2013-05-09")));
-		books.add(new Book(6, "php", 40d, "编程语言", LocalDate.parse("2014-08-06")));
-		books.add(new Book(7, "html", 44d, "编程语言", LocalDate.parse("2011-01-06")));
-		books.add(new Book(8, "oracle", 150d, "数据库", LocalDate.parse("2013-08-09")));
-		books.add(new Book(9, "mysql", 66d, "数据库", LocalDate.parse("2015-04-06")));
-		books.add(new Book(10, "ssh", 70d, "编程语言", LocalDate.parse("2016-12-04")));
-		books.add(new Book(11, "设计模式", 81d, "其他", LocalDate.parse("2017-04-06")));
-		books.add(new Book(12, "重构", 62d, "其他", LocalDate.parse("2012-04-09")));
-		books.add(new Book(13, "敏捷开发", 72d, "其他", LocalDate.parse("2016-09-07")));
-		books.add(new Book(14, "从技术到管理", 42d, "其他", LocalDate.parse("2016-02-19")));
-		books.add(new Book(15, "算法导论", 66d, "其他", LocalDate.parse("2010-05-08")));
-		books.add(new Book(16, "oracle 12c", 150d, "数据库", LocalDate.parse("2017-05-08")));
-		return books;
-	}
+    }
+
+    @Test
+    public void test9() {
+        books().stream().filter(book -> book.getPrice() >= 80).sorted(Comparator.comparing(Book::getPublishDate).reversed()).forEach(System.out::println);
+    }
+
+    /**
+     * 多个字段groupBy
+     */
+    @Test
+    public void test10() {
+
+        Map<String, Map<String, Double>> collect = books.stream().collect(Collectors.groupingBy(Book::getAuthor, Collectors.groupingBy(Book::getType, Collectors.summingDouble(Book::getPrice))));
+        System.out.println(collect);
+    }
+    private List<Book> books=books();
+    private List<Book> books() {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1, "tomcat", "张三", 70d, "服务器", LocalDate.parse("2014-05-17")));
+        books.add(new Book(2, "jetty", "李四", 60d, "服务器", LocalDate.parse("2015-12-01")));
+        books.add(new Book(3, "nginx", "张三", 65d, "服务器", LocalDate.parse("2016-10-17")));
+        books.add(new Book(4, "java", "李四", 66d, "编程语言", LocalDate.parse("2011-04-09")));
+        books.add(new Book(5, "ruby", "张三", 80d, "编程语言", LocalDate.parse("2013-05-09")));
+        books.add(new Book(6, "php", "张三", 40d, "编程语言", LocalDate.parse("2014-08-06")));
+        books.add(new Book(7, "html", "王五", 44d, "编程语言", LocalDate.parse("2011-01-06")));
+        books.add(new Book(8, "oracle", "张三", 150d, "数据库", LocalDate.parse("2013-08-09")));
+        books.add(new Book(9, "mysql", "王五", 66d, "数据库", LocalDate.parse("2015-04-06")));
+        books.add(new Book(10, "ssh", "张三", 70d, "编程语言", LocalDate.parse("2016-12-04")));
+        books.add(new Book(11, "设计模式", "李四", 81d, "其他", LocalDate.parse("2017-04-06")));
+        books.add(new Book(12, "重构", "李四", 62d, "其他", LocalDate.parse("2012-04-09")));
+        books.add(new Book(13, "敏捷开发", "张三", 72d, "其他", LocalDate.parse("2016-09-07")));
+        books.add(new Book(14, "从技术到管理", "王五", 42d, "其他", LocalDate.parse("2016-02-19")));
+        books.add(new Book(15, "算法导论", "张三", 66d, "其他", LocalDate.parse("2010-05-08")));
+        books.add(new Book(16, "oracle 12c", "李四", 150d, "数据库", LocalDate.parse("2017-05-08")));
+        return books;
+    }
 }
